945376cc04bc6337f8d5e8507bc7fc30f81c1986
David Pinney <david@pinney.org>
2020-03-17 17:30:29 -0400

    working around splat's wonky pathing

--- install.py
+++ install.py
@@ -10,5 +10,8 @@ if platform.system()=="Darwin": # MacOS
 	# SPLAT TEST
 	os.system("wget https://www.qsl.net/kd2bd/splat-1.4.2-osx.tgz")
 	os.system("sudo tar -xvzf splat-1.4.2-osx.tgz")
-	os.system('''sed -i '' 's/ans=""/ans="2"/g' splat-1.4.2/configure''')
-	os.system("sudo bash splat-1.4.2/configure")
+	os.system('''
+		cd splat-1.4.2
+		sed -i '' 's/ans=""/ans="2"/g' configure;
+		sudo bash configure;
+	''')
