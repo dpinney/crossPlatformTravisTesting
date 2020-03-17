d5ffff3550af0cf66139d41595c43d4641daa926
David Pinney <david@pinney.org>
2020-03-17 10:40:36 -0400

    workaround for python3 death on macos

--- install.py
+++ install.py
@@ -4,4 +4,6 @@ print('INSTALL RUNNING')
 
 # macos brew testing
 if platform.system()=="Darwin": # MacOS
-	os.system("brew install wget ffmpeg git graphviz octave mdbtools")
+	# os.system("brew install wget ffmpeg git graphviz octave mdbtools")
+	# os.system("brew install wget ffmpeg git graphviz octave mdbtools; brew link --overwrite python3")
+	os.system("HOMEBREW_NO_AUTO_UPDATE=1 brew install wget ffmpeg git graphviz octave mdbtools")
