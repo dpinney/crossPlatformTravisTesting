5921fa4fcc90b435059a4a1bab6a44fd545d90b2
David Pinney <david@pinney.org>
2020-03-17 12:17:04 -0400

    splat testing exec fix

--- install.py
+++ install.py
@@ -4,6 +4,10 @@ print('INSTALL RUNNING')
 
 # macos brew testing
 if platform.system()=="Darwin": # MacOS
-	# os.system("brew install wget ffmpeg git graphviz octave mdbtools")
-	os.system("brew install wget ffmpeg git graphviz octave mdbtools; brew link --overwrite python3")
-	# os.system("HOMEBREW_NO_AUTO_UPDATE=1 brew install wget ffmpeg git graphviz octave mdbtools")
+	# os.system("brew install wget ffmpeg git graphviz octave mdbtools") # fails, kills python
+	# os.system("brew install wget ffmpeg git graphviz octave mdbtools; brew link --overwrite python3") # works, extra 5 minutes
+	os.system("HOMEBREW_NO_AUTO_UPDATE=1 brew install wget ffmpeg git graphviz octave mdbtools") # works, fast
+	# SPLAT TEST
+	os.system("wget https://www.qsl.net/kd2bd/splat-1.4.2-osx.tgz")
+	os.system("sudo tar -xvzf splat-1.4.2-osx.tgz")
+	os.system("sudo bash splat-1.4.2/configure")
