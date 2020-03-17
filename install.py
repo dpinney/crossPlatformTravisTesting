1c843a5136ed301a485e9a1fc6cf75f26f49ecc6
David Pinney <david@pinney.org>
2020-03-17 10:20:26 -0400

    trying to break macos with somethign benign

--- install.py
+++ install.py
@@ -1,2 +1,7 @@
+import platform, os
 #  install script
 print('INSTALL RUNNING')
+
+# macos brew testing
+if platform.system()=="Darwin": # MacOS
+	os.system("brew install wget ffmpeg git graphviz octave mdbtools")
