b697f8f45e86fd398c1258b4b0e6f8ea3c6929c5
David Pinney <david@pinney.org>
2020-03-19 11:04:24 -0400

    Why oh why does this install fail to do this

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ print('TEST SCRIPT RUN')
 
 if platform.system()=="Windows":
 	print('WINDOWS DETECTED')
+	os.environ["GLPATH"] = "C:\\Program Files\\GridLAB-D\\bin;C:\\Program Files\\GridLAB-D\\etc;C:\\Program Files\\GridLAB-D\\lib;C:\\Program Files\\GridLAB-D\\samples;C:\\Program Files\\GridLAB-D\\rt;C:\\Program Files\\GridLAB-D\tmy"
 	p = subprocess.Popen(['gridlabd', 'smsSingle.glm'])
 	p.wait()
 	print(p.returncode)
