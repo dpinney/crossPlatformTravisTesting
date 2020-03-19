7b5b901d0bd9c037a5012441cc24a8e244571667
David Pinney <david@pinney.org>
2020-03-19 11:07:53 -0400

    more path nonsense

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ print('TEST SCRIPT RUN')
 
 if platform.system()=="Windows":
 	print('WINDOWS DETECTED')
-	os.environ["GLPATH"] = "C:\\Program Files\\GridLAB-D\\bin;C:\\Program Files\\GridLAB-D\\etc;C:\\Program Files\\GridLAB-D\\lib;C:\\Program Files\\GridLAB-D\\samples;C:\\Program Files\\GridLAB-D\\rt;C:\\Program Files\\GridLAB-D\tmy"
+	print('RAW GLD RUN', os.system('gridlabd -h'))
 	p = subprocess.Popen(['gridlabd', 'smsSingle.glm'])
 	p.wait()
 	print(p.returncode)
