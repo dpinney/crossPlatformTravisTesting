f287ad8c307fb9df43f04cea6b97190f9d285055
David Pinney <david@pinney.org>
2020-03-19 12:18:44 -0400

    the level of debug is out of control

--- script.py
+++ script.py
@@ -2,11 +2,19 @@
 import platform, os, subprocess
 print('TEST SCRIPT RUN')
 
+def safe_call(cmdString):
+	try:
+		output = subprocess.check_output(
+			cmdString, stderr=subprocess.STDOUT, shell=True,
+			universal_newlines=True)
+	except subprocess.CalledProcessError as exc:
+		print(cmdString + " Status : FAIL", exc.returncode, exc.output)
+	else:
+		print(cmdString + " Output: \n{}\n".format(output))
+
 if platform.system()=="Windows":
 	print('WINDOWS DETECTED')
 	print('PATH', os.getenv('PATH'))
 	print('GLPATH', os.getenv('GLPATH'))
-	saneOut = subprocess.check_output('gridlabd -h')
-	print('Just open gld:', saneOut)
-	realOut = subprocess.check_output('gridlabd smsSingle.glm')
-	print('Running GLM:', realOut)
+	safe_call('gridlabd -h')
+	safe_call('gridlabd smsSingle.glm')
