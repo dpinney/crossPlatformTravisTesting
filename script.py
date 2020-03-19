374c95b4ecc60e7e59a0e66f0e1d03c9c380b555
David Pinney <david@pinney.org>
2020-03-19 11:12:44 -0400

    Show me more env vars

--- script.py
+++ script.py
@@ -4,6 +4,8 @@ print('TEST SCRIPT RUN')
 
 if platform.system()=="Windows":
 	print('WINDOWS DETECTED')
+	os.system('echo Env:GLPATH')
+	os.system('echo Env:PATH')
 	print('RAW GLD RUN', os.system('gridlabd -h'))
 	p = subprocess.Popen(['gridlabd', 'smsSingle.glm'])
 	p.wait()
