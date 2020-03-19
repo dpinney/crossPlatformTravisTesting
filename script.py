635047628607d47bfd3d9b5ceb32bd44fa93dcf7
David Pinney <david@pinney.org>
2020-03-19 11:50:51 -0400

    better environment printing

--- script.py
+++ script.py
@@ -4,8 +4,8 @@ print('TEST SCRIPT RUN')
 
 if platform.system()=="Windows":
 	print('WINDOWS DETECTED')
-	os.system('echo $GLPATH')
-	os.system('echo $PATH')
+	print('PATH', os.getenv('PATH'))
+	print('PATH', os.getenv('GLPATH'))
 	print('RAW GLD RUN', os.system('gridlabd -h'))
 	p = subprocess.Popen(['gridlabd', 'smsSingle.glm'])
 	p.wait()
